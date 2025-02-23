8128a80f8bb64b13661d6c774008bc803d082237
Jini <wlsdl6685@gmail.com>
2025-02-23 22:51:26 +0900

    feat: 도서 페이지

--- book-store-v/src/App.tsx
+++ book-store-v/src/App.tsx
@@ -7,6 +7,7 @@ import Error from "./components/common/Error";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassword";
 import Login from "./pages/Login";
+import Books from "./pages/Books";
 
 const router = createBrowserRouter([
   {
@@ -28,7 +29,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/books',
-    element: <Layout><div>도서 목록</div></Layout>
+    element: <Layout><Books /></Layout>
   }
 ])
 function App() { 
--- book-store-v/src/hooks/useCategory.ts
+++ book-store-v/src/hooks/useCategory.ts
@@ -1,9 +1,34 @@
 import { useEffect, useState } from "react";
 import { Category } from "../models/category.model";
 import { fetchCategory } from "../api/category.api";
+import { useLocation } from "react-router-dom";
 
 export const useCategory = () => {
+  const location = useLocation();
   const [category, setCategory] = useState<Category[]>([]);
+
+  const setActive = () => {
+    const params = new URLSearchParams(location.search);
+    if (params.get("category_id")) {
+      setCategory((prev) => {
+        return prev.map((item) => {
+          return {
+            ...item,
+            isActvie: item.id === Number(params.get("category_id")),
+          };
+        });
+      });
+    } else {
+      setCategory((prev) => {
+        return prev.map((item) => {
+          return {
+            ...item,
+            isActvie: false
+          };
+        });
+      });
+    }
+  };
 
   useEffect(() => {
     fetchCategory().then((category) => {
@@ -12,8 +37,13 @@ export const useCategory = () => {
       const categoryWithAll = [{ id: null, name: "전체" }, ...category];
 
       setCategory(categoryWithAll);
+      setActive();
     });
   }, []);
+
+  useEffect(()=> {
+    setActive();
+  }, [location.search]);
 
   return { category };
 };
